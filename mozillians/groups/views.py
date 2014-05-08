49b5047a4e0763e9d38f62e580912ee995eecd25
Wray Bowling <wbowling@caktusgroup.com>
2014-05-08 16:35:43 -0400

    labels were spewing out related objects like “Mountain View, California, United States, California, United States, United States”

--- mozillians/groups/views.py
+++ mozillians/groups/views.py
@@ -134,7 +134,7 @@ def show(request, url, alias_model, template):
     geodata = []
     for profile in profiles:
         if profile.lat and profile.lng:
-            labelText = "%s - %s, %s, %s" % (profile.ircname, profile.geo_city, profile.geo_region, profile.geo_country)
+            labelText = "%s - %s, %s, %s" % (profile.ircname, profile.geo_city.name, profile.geo_region.name, profile.geo_country.name)
             geodata.append(dict([("lat", profile.lat), ("lng", profile.lng), ("labelText", labelText)]))
 
     page = request.GET.get('page', 1)
